7c30a95c66b4873e5333fbf459f90597e44f2412
Filipe Mota <filipera3@gmail.com>
2025-04-03 15:24:00 -0300

    initial commit

---
+++ src/test/java/br/com/filipecode/DeskhelpApi/DeskhelpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.filipecode.DeskhelpApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskhelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
