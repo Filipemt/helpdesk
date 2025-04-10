9830a4462aa7d0f38c7090a85637fd4587602e7a
Filipe Mota <filipera3@gmail.com>
2025-04-10 11:13:55 -0300

    feat: adiciona validação de alteração em chamados concluídos

---
+++ src/main/java/br/com/filipecode/DeskhelpApi/chamado/validator/ChamadoValidador.java
@@ -0,0 +1,30 @@
+package br.com.filipecode.DeskhelpApi.chamado.validator;
+
+import br.com.filipecode.DeskhelpApi.chamado.entity.Chamado;
+import br.com.filipecode.DeskhelpApi.chamado.repository.ChamadoRepository;
+
+import br.com.filipecode.DeskhelpApi.shared.enums.Status;
+import br.com.filipecode.DeskhelpApi.shared.exceptions.EntidadeNaoEncontradaException;
+import br.com.filipecode.DeskhelpApi.shared.exceptions.RequisicaoInvalidadeException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class ChamadoValidador {
+
+    private final ChamadoRepository chamadoRepository;
+
+    public Chamado validarChamadoPodeSerAlterado(UUID id) {
+        Chamado chamado = chamadoRepository.findById(id)
+                .orElseThrow(() -> new EntidadeNaoEncontradaException("Chamado não encontrado!"));
+
+        if (chamado.getStatus() == Status.CONCLUIDO) {
+            throw new RequisicaoInvalidadeException("Chamado já foi concluido e não pode ser alterado!");
+        }
+
+        return chamado;
+    }
+}
