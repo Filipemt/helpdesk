9a21f9d53e6cdedb77ceeb1628f91f32b75858ac
Filipe Mota <filipera3@gmail.com>
2025-07-03 08:05:52 -0300

    refactor: remove a entidade Tecnico

--- src/main/java/br/com/filipecode/DeskhelpApi/legacy/tecnico/dto/TecnicoDTO.java
+++
@@ -1,9 +0,0 @@
-package br.com.filipecode.DeskhelpApi.legacy.tecnico.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record TecnicoDTO(
-                        @NotBlank(message = "Nome é obrigatório!") String nome,
-                        @NotBlank(message = "Email é obrigatório!") String email,
-                        @NotBlank(message = "Especialização é obrigatório!") String especializacao) {
-}
